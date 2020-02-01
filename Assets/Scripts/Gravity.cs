fc7b6e5d2984e8edf61ac38d8674efc3ef8f8d1d
Juhana Kekoni <juhanakekoni@yahoo.com>
2020-02-01 19:08:12 +0200

    Gravity.cs: Add basic gravity logic

--- Assets/Scripts/Gravity.cs
+++ Assets/Scripts/Gravity.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Gravity : MonoBehaviour
 {
+
+    private int gravity_count = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,33 @@ public class Gravity : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Debug.Log("Gravity count" + gravity_count);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponentInChildren<Room>())
+        {
+            if (gravity_count++ == 0)
+            {
+                GetComponentInChildren<Movement>().SetGravity(true);
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.GetComponentInChildren<Room>())
+        {
+            --gravity_count;
+            if (gravity_count < 0)
+            {
+                gravity_count = 0;
+            }
+            if (gravity_count == 0)
+            {
+                GetComponentInChildren<Movement>().SetGravity(false);
+            }
+        }
     }
 }
